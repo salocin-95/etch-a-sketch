67eb83bae31a0564d7072b3438fdb111a26097f5
salocin-95 <renicolas7@gmail.com>
2024-10-26 00:51:02 +0000

    Revert "Modify all the files"
    This reverts commit 57eb45440aa7de3457d63f2563698be8e23f24b6.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const createGridElement = () => {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-element")
     divContainer.appendChild(gridElement);
-    gridElement.innerHTML = ""
+    gridElement.innerHTML = `IM A DIV ${j} times ${i}`;
 }
 
 const createGrid = () => {
@@ -17,19 +17,6 @@ const createGrid = () => {
 }
 
 
-
 console.log("imlinked")
 console.log(divContainer)
-
 createGrid()
-
-// Need a for loop to iterate through gridELement because it is an array
-// Need to change gridElement name because I've already used it
-
-/*const gridElement = document.querySelectorAll(".grid-element")
-
-const changeColor = () => {
-    gridElement.classList.add("black");
-}
-
-gridElement.addEventListener('click', changeColor());*/
