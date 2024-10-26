738abe7a83e8027ac6e28cdbc5d4ef4c096384e5
salocin-95 <renicolas7@gmail.com>
2024-10-26 03:54:58 +0000

    Modify files and add functionality

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-const divContainer = document.querySelector(".container");
-
+const divContainer = document.querySelector(".grid-container");
+const clearBtn = document.getElementById("clear")
 
 const createGridElement = () => {
     const gridElement = document.createElement("div");
-    gridElement.classList.add("grid-element")
+    gridElement.classList.add("grid-element");
     divContainer.appendChild(gridElement);
-    gridElement.innerHTML = `IM A DIV ${j} times ${i}`;
 }
+
+const clearSketch = () => {
+    document.querySelectorAll(".grid-element-background").forEach((element) => element.classList.remove("grid-element-background"));
+}
+
+// For every gridElement it should have a class that matches with an X and Y number so when the mouse is over the it changes the color according to its coordinates
 
 const createGrid = () => {
     for (i = 1; i <= 16; i++) {  
         for (j = 1; j <= 16;j++) {
             createGridElement();
-        }
+        };
+    };
+};
+
+
+console.log("imlinked");
+console.log(divContainer);
+createGrid();
+
+const gridElement = document.querySelector(".grid-element");
+
+console.log(gridElement);
+console.log(document.querySelector(".grid-element"))
+
+// If .grid-element is added to the DOM dynamically after the initial page load, the click event listener won’t attach. You can use event delegation for elements that are added later:
+
+// Should have and embedded templates that changes the color of the grid selected X-Y coordinate 
+document.addEventListener("mouseover", (event) => {
+    if (event.target.matches(".grid-element")) {
+        // event.target matches the div .grid-element mouseover
+        event.target.classList.add("grid-element-background");
+        // console.log("you clicked bro");
     }
-}
+});
 
+clearBtn.addEventListener("click", clearSketch);
 
-console.log("imlinked")
-console.log(divContainer)
-createGrid()
+// Targets mouse poisiton
+// onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}
