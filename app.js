2b209c0171df796cbd9a77462b351f3485fb3a20
salocin-95 <renicolas7@gmail.com>
2024-10-30 20:47:07 +0000

    Main functionality is working

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
+// Batch DOM updates to make the slider perform faster
 const divContainer = document.querySelector(".grid-container");
 const clearBtn = document.getElementById("clear");
+const eraserBtn = document.getElementById("eraser");
+const drawBtn = document.getElementById("draw");
+const rainbowBtn = document.getElementById("rainbow");
+const shadowBtn = document.getElementById("darken");
 const gridSize = document.getElementById("grid-size");
 const gridSizeDisplay = document.querySelector(".grid-size-display");
+let size = parseInt(gridSize.value)
 
 const clearSketch = () => {
     document.querySelectorAll(".grid-element-background").forEach((element) => element.classList.remove("grid-element-background"));
 }
 
-// For every gridElement it should have a class that matches with an X and Y number so when the mouse is over the it changes the color according to its coordinates
+//Changes the size of the grid
 const createGrid = () => {
-    for (i = 1; i <= parseInt(gridSize.value); i++) {  
-        for (j = 1; j <= parseInt(gridSize.value);j++) {
+    size = parseInt(gridSize.value)
+    divContainer.innerHTML = "";
+    for (let i = 1; i <= size; i++) {  
+        for (let j = 1; j <= size; j++) {
             createGridElement();
         };
     };
+    gridSizeDisplay.innerHTML = `<span>${size}x${size}</span>`
 }
 
+// Creates the grid element. Changes should be made to make the DOM update in batches
 const createGridElement = () => {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-element");
     divContainer.appendChild(gridElement);
-    gridElement.style.height = `${(660/(parseInt(gridSize.value)).toFixed(2))}px`
-    gridElement.style.width = `${(660/(parseInt(gridSize.value)).toFixed(2))}px`
+    gridElement.style.height = `${(660/(size).toFixed(2))}px`
+    gridElement.style.width = `${(660/(size).toFixed(2))}px`
 }
 
-    /*gridElement.style.height = `${gridSize.value}px`
-    gridElement.style.width = `${gridSize.value}px`*/
-
+// Changes the grid size value display
 const gridSizeValue = () => {
-    console.log(gridSize.value);
     gridSizeDisplay.innerHTML = `<span>${gridSize.value}x${gridSize.value}</span>`
-    console.log(660/(parseInt(gridSize.value)).toFixed(2))
 }
-
-createGrid();
-
-const gridElement = document.querySelector(".grid-element");
-
 
 // If .grid-element is added to the DOM dynamically after the initial page load, the click event listener won’t attach. You can use event delegation for elements that are added later:
 
@@ -49,7 +50,26 @@ document.addEventListener("mouseover", (event) => {
     }
 });
 
-clearBtn.addEventListener("click", clearSketch);
+eraserBtn.addEventListener("click", () => {
+    document.addEventListener("mouseover", (event) => {
+        if (event.target.matches(".grid-element")) {
+            // event.target matches the div .grid-element mouseover
+            event.target.classList.remove("grid-element-background");
+            // console.log("you clicked bro");
+        }
+    });
+});
 
-// Targets mouse poisition
-// onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}
+drawBtn.addEventListener("click", () => {
+    document.addEventListener("mouseover", (event) => {
+        if (event.target.matches(".grid-element")) {
+            // event.target matches the div .grid-element mouseover
+            event.target.classList.add("grid-element-background");
+            // console.log("you clicked bro");
+        }
+    });
+});
+
+
+clearBtn.addEventListener("click", clearSketch);
+createGrid();
